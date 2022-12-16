43355e32ef130fcc510939fa467cbbd3bca1c78a
Richard <72392252+richardcollinsDkIT@users.noreply.github.com>
2022-12-16 22:26:15 +0000

    Added image preprocessing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 from os import listdir
 
 def read_files(folder):
@@ -8,6 +9,20 @@ def read_files(folder):
       for image in os.listdir(folder_dir):
             images.append(image)
       return images
+
+def grey_scale(image):
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      return image
+
+def equalise(image):
+      image = cv2.equalizeHist(image)
+      return image
+
+def preprocess_image(image):
+      image = grey_scale(image)
+      image = equalise(image)
+      image = image/255
+      return image
 
 if __name__ == "__main__":
       test_images = read_files("test")
