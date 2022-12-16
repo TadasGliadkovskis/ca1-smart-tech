4b4adfbeeeb47a378d0b0e0e55066e6607fa8630
Ted <tadas2410@gmail.com>
2022-12-16 20:50:24 +0000

    Read test val images

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import os
+from os import listdir
+
+def read_files(folder):
+# get the path/directory
+      folder_dir = "C:/smartTech/ca1-smart-tech/tiny-imagenet-200/"+folder+"/images"
+      images =[]
+      for image in os.listdir(folder_dir):
+            images.append(image)
+      return images
+
 if __name__ == "__main__":
-   print("In main")
+      test_images = read_files("test")
+      validation_images = read_files("val")
+      print(len(validation_images))
